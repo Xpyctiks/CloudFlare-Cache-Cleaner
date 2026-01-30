11579dc82447eaa4f5827320a334969d9f835678
Ruslan Sieriebriannikov <xpyct@xpyct.com>
2026-01-30 11:00:17 +0200

    Logging change from DEBUG to INFO

--- functions/load_config.py
+++ functions/load_config.py
@@ -15,7 +15,7 @@ def load_config(application) -> None:
         "LOG_FILE": f"{config.logFile}",
         "ENCRYPT_KEY": f"{config.encryptKey}"
       })
-      logging.basicConfig(filename=application.config["LOG_FILE"],level=logging.DEBUG,format='%(asctime)s - Cloudflare-Cache-Cleaner - %(levelname)s - %(message)s',datefmt='%d-%m-%Y %H:%M:%S')
+      logging.basicConfig(filename=application.config["LOG_FILE"],level=logging.INFO,format='%(asctime)s - Cloudflare-Cache-Cleaner - %(levelname)s - %(message)s',datefmt='%d-%m-%Y %H:%M:%S')
       logging.getLogger('werkzeug').setLevel(logging.WARNING)
     except Exception as msg:
       print(f"Load-config error: {msg}")
